dbb06990cb710b229fb4ac24c11139e4b83aa2e8
Enigma-RedM <crimsonroleplay@outlook.com>
2024-11-08 03:30:08 +1000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version "adamant"
 game "rdr3"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'Shark'
-version '0.1.5'
+author 'Enigma'
+version '1.0'
 
 ui_page 'index.html'
 
